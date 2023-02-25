0a672d617ec54515abed33d42143d7cd5b9130a9
Beto Zarzoza <betozarzoza@gmail.com>
2023-02-24 22:35:17 -0600

    fix commands

--- routes/botman.php
+++ routes/botman.php
@@ -10,12 +10,24 @@ $botman->hears('.*opciones.*', function ($bot) {
     $bot->startConversation(new GeneralOptionsConversation);
 });
 
+$botman->hears('hola', function ($bot) {
+    $bot->startConversation(new GeneralOptionsConversation);
+});
+
+$botman->hears('Hola', function ($bot) {
+    $bot->startConversation(new GeneralOptionsConversation);
+});
+
 
 $botman->hears('.*cuanto debo.*', function($bot) {
     $bot->startConversation(new ExampleConversation);
 });
 
 $botman->hears('admin', function($bot) {
+    $bot->startConversation(new AddMovementConversation);
+});
+
+$botman->hears('Admin', function($bot) {
     $bot->startConversation(new AddMovementConversation);
 });
 
